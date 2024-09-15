76234f5208a7337fb84417fdf6ae1474b79b1c60
ramaherdianto <ramadhanherdianto35@gmail.com>
2024-09-15 16:16:42 +0700

    feat: add user posting and aside user suggestions and footer, all responsive

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,10 @@
 const menuToggle = document.querySelector('.menu');
 const nav = document.querySelector('nav ul');
+const header = document.querySelector('.navbar-container');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 50);
+});
 
 menuToggle.addEventListener('click', function () {
     nav.classList.toggle('mobile');
