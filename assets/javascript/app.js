0e83d0fddca3f063c80687d0ec10961ae7461cbf
ramaherdianto <ramadhanherdianto35@gmail.com>
2024-09-15 16:59:24 +0700

    feat: add user input for dynamic username

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,14 @@
 const menuToggle = document.querySelector('.menu');
 const nav = document.querySelector('nav ul');
 const header = document.querySelector('.navbar-container');
+const username = document.querySelector('#user-username');
+
+const userInput = prompt('Masukkan username instagram kamu !');
+if (userInput) {
+    username.textContent = userInput;
+} else {
+    username.textContent = 'Your Profile';
+}
 
 window.addEventListener('scroll', () => {
     header.classList.toggle('shadow', window.scrollY > 50);
